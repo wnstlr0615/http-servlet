976078098248cb0df4380790203216ba9e43f4e4
wnstlr0615 <76856000+wnstlr0615@users.noreply.github.com>
2021-09-16 14:51:48 +0900

    HttoSevletRquestBodystring HttpServletRequestjson 데이터 받기

---
+++ src/main/java/hello/servket/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,28 @@
+package hello.servket.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servket.basic.HelloData;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+    private ObjectMapper mapper=new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String messageBody = StreamUtils.copyToString(
+                req.getInputStream(), StandardCharsets.UTF_8
+        );
+        System.out.println("messageBody = " + messageBody);
+        HelloData helloData = mapper.readValue(messageBody, HelloData.class);
+        System.out.println("helloData = " + helloData);
+    }
+
+}
