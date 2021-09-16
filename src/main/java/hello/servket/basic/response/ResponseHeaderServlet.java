1d1e2ef6023c1e3eaf90b9f1e26ec1625a99e9ef
wnstlr0615 <76856000+wnstlr0615@users.noreply.github.com>
2021-09-16 15:13:02 +0900

    HttpResponseServlet 파일 구조 보기

---
+++ src/main/java/hello/servket/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,57 @@
+package hello.servket.basic.response;
+
+import org.springframework.http.HttpHeaders;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name ="responseHeaderServlet" , urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setStatus(HttpServletResponse. SC_BAD_REQUEST);
+
+        resp.setHeader(HttpHeaders.CONTENT_TYPE, "text/pain;charset=utf-8");
+        resp.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+        resp.setHeader(HttpHeaders.PRAGMA, "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        content(resp);
+        cookie(resp);
+        redirect(resp);
+
+        resp.getWriter().println("ok");
+
+    }
+
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
